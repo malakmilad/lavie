fe44d2c8c726d618c5931a73a31d608befdf2fea
Malak Milad <malakmilad202@gmail.com>
2025-11-15 15:01:35 +0200

    model

---
+++ app/Models/ContactRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactRequest extends Model
+{
+    protected $table = 'contact_requests';
+
+    protected $fillable = [
+        'fullname',
+        'phone',
+        'email',
+        'message',
+    ];
+}
